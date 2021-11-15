d911447ef7b25ef3fadaae7e2d20e9b44afa87a4
Gengiskan <91074795+ForMyFreedom@users.noreply.github.com>
2021-11-15 11:05:50 -0300

    Change the Rock emoji

--- RockPaperScissor/Data/CardCreator.cs
+++ RockPaperScissor/Data/CardCreator.cs
@@ -6,9 +6,9 @@ namespace RockPaperScissor.Data
     {
         static public void GetStandartCardsToDeck(Deck deck)
         {
-            AddALotOFCardsToDeck(deck, "Pedra", new[] { 1, 0, 0 }, 0, 5);
-            AddALotOFCardsToDeck(deck, "Papel", new[] { 0, 0, 1 }, 0, 5);
-            AddALotOFCardsToDeck(deck, "Tesoura", new[] { 0, 1, 0 }, 0, 5);
+            AddALotOFCardsToDeck(deck, "⛰️", new[] { 1, 0, 0 }, 0, 5);
+            AddALotOFCardsToDeck(deck, "📜", new[] { 0, 0, 1 }, 0, 5);
+            AddALotOFCardsToDeck(deck, "✂️", new[] { 0, 1, 0 }, 0, 5);
         }
 
 
