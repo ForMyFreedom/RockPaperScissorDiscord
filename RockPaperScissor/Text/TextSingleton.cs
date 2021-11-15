baff740a3107943df7744a4a095ab6f4479aaca0
Gengiskan <91074795+ForMyFreedom@users.noreply.github.com>
2021-11-15 11:05:38 -0300

    Change class name to a better one

--- RockPaperScissor/Text/TextSingleton.cs
+++ RockPaperScissor/Text/TextSingleton.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DSharpPlus.Entities;
+using RockPaperScissor.Data;
 
 namespace RockPaperScissor.Text
 {
-    public class TextGerenciatorSingleton
+    public class TextSingleton
     {
         public static Dictionary<String, TextMessagesGerenciator> GerenciatorDictionary = new Dictionary<string, TextMessagesGerenciator>()
         {
@@ -28,6 +30,21 @@ namespace RockPaperScissor.Text
                 return GerenciatorDictionary.FirstOrDefault(x => x.Value == gerenciator).Key;
             else
                 return "en";
+        }
+
+        public static TextMessagesGerenciator GetMemberGerenciator(DiscordMember member)
+        {
+            return GetGerenciator(AllGameData.GetMemberDeck(member).GetLanguage());
+        }
+
+        public static TextMessagesGerenciator GetMemberGerenciator(ulong id)
+        {
+            return GetGerenciator(AllGameData.GetMemberDeck(id).GetLanguage());
+        }
+
+        public static TextMessagesGerenciator GetMemberGerenciator(Deck deck)
+        {
+            return GetGerenciator(deck.GetLanguage());
         }
     }
 }
